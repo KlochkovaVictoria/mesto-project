ee573a83765a913e625f47a2271849c411a72619
Victoria_Klochkova <klochkovavictoria@yandex.ru>
2022-08-09 14:39:47 +0300

    fix

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ let closeIcon = popup.querySelector('.popup__close-icon');
 let profile = document.querySelector('.profile');
 let editButton = profile.querySelector('.profile__edit-button');
 //cards
-let mainContent = document.querySelector('.main-content');
-let cards = mainContent.querySelector('.cards');
-let cardsItem = cards.querySelectorAll('.cards__item');
-let cardsCaption = cardsItem.querySelectorAll('.cards__caption');
-let likeIcon = cardsCaption.querySelectorAll('.cards__like-icon');
+
+
+let cardsItem = document.querySelectorAll('.cards__item');
+let likeIcon = document.querySelector('.cards__like-icon');
 
 /*открытие окна редактирования popup по кнопке edit*/
 editButton.addEventListener('click', function() {
@@ -22,6 +21,14 @@ closeIcon.addEventListener('click', function() {
 });
 
 /*нажатие на кнопку like*/
+cardsItem.forEach(function(likeIcon) {
+  likeIcon.addEventListener('click', function(ev) {
+    ev.stopPropagation();
+    likeIcon.classList.toggle('cards__like-icon_active');
+  });
+});
+
+
 likeIcon.addEventListener('click', function() {
   likeIcon.classList.toggle('cards__like-icon_active');
 });
