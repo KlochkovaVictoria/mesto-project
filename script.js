36630073461c99313a23bdcc3a91b70c216c62b3
Victoria_Klochkova <klochkovavictoria@yandex.ru>
2022-08-09 16:54:00 +0300

    fix

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ let editButton = profile.querySelector('.profile__edit-button');
 //cards
 
 
+let cardsItem = document.querySelectorAll('.cards__item');
+let likeIcons = document.querySelectorAll('.cards__like-icon');
+
 /*открытие окна редактирования popup по кнопке edit*/
 editButton.addEventListener('click', function() {
   popup.classList.toggle('popup_opened');
@@ -15,4 +18,11 @@ editButton.addEventListener('click', function() {
 /*закрытие окна редактирования popup по иконке close*/
 closeIcon.addEventListener('click', function() {
   popup.classList.toggle('popup_opened');
+});
+
+/*нажатие на кнопку like*/
+likeIcons.forEach(function(likeIcon) {
+  likeIcon.addEventListener('click', function(ex) {
+    likeIcon.classList.toggle('cards__like-icon_active');
+  });
 });
